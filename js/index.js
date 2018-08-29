7c4368b72f22b1f5f7e61449626bda18ed418f9d
Antonio <assitaliroma@gmail.com>
2018-08-29 08:30:24 -0300

    FORM:
    - Adding Type field to the form since it's needed on the payload
    - Remove success and error method from jquery.ajax
    - Use complete method since there we can evaluate the response status to show the correct alert

--- js/index.js
+++ js/index.js
@@ -29,26 +29,14 @@ $(function () {
         dataType: "json",
         processData: false,
         contentType: "application/json",
-        success: function (data) {
-          var messageAlert = "alert-success";
-          var messageText = "We will contact you soon!"
-
-          // let's compose Bootstrap alert box HTML
-          var alertBox = '<div class="alert ' + messageAlert + ' alert-dismissable"><button type="button" class="close" data-dismiss="alert" aria-hidden="true">&times;</button>' + messageText + '</div>';
-
-          // If we have messageAlert and messageText
-          if (messageAlert && messageText) {
-            // inject the alert to .messages div in our form
-            $('#contact-form-obj').find('.messages').html(alertBox);
-            // empty the form
-            $('#contact-form-obj')[0].reset();
+        complete: function (e, xhr, settings) {
+          var messageAlert = "alert-danger"
+          var messageText = "We have problems at this moment. Please try again later";
+          if (e.status === 200 || e.status === 201) {
+            messageAlert = "alert-success";
+            messageText = "We will contact you soon!"
           }
-        },
-        error: function (error) {
-          if (error) {
-            var messageAlert = "alert-danger"
-            var messageText = "We have problems at this moment. Please try again later";
-          }
+
           // let's compose Bootstrap alert box HTML
           var alertBox = '<div class="alert ' + messageAlert + ' alert-dismissable"><button type="button" class="close" data-dismiss="alert" aria-hidden="true">&times;</button>' + messageText + '</div>';
 
